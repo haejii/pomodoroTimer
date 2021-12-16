66f19cc6d475dbfcbd5213759fe22679b6bf08a9
haejii <yanghagi@naver.com>
2021-12-16 09:28:06 +0900

    initail

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by 양혜지 on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
